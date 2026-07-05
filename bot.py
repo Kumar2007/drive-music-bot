394aec275fbaa14844d05e13f28e9307224a2e4a
Kumar2007 <kumar2007sanchit@gmail.com>
2026-07-05 14:26:06 +0530

    Move FOLDER_ID configuration to environment variable fallback

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ except Exception as e:
     drive_manager = None
 
 # Hardcoded Google Drive Folder ID containing your audio tracks
-FOLDER_ID = "1koYriiJvKEGIM-WvyEJShRArzi-tuHYV"
+FOLDER_ID = os.environ.get("DRIVE_FOLDER_ID", "1koYriiJvKEGIM-WvyEJShRArzi-tuHYV")
 
 @bot.event
 async def on_ready():
